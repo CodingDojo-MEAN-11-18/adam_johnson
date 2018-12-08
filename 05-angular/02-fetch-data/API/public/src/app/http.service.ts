2857cbae8e6da7c966e1defc3c84405f6cf38588
Adam Johnson <adam.johnson311@gmail.com>
2018-12-08 11:25:00 -0600

    finished pokemon api

--- 05-angular/02-fetch-data/API/public/src/app/http.service.ts
+++ 05-angular/02-fetch-data/API/public/src/app/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, getModuleFactory } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { getUrlScheme } from '../../node_modules/@angular/compiler';
+import { getUrlScheme, ArrayType } from '../../node_modules/@angular/compiler';
+import { getInheritedFactory } from '../../node_modules/@angular/core/src/render3';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,24 @@ export class HttpService {
   getPokemon(){
     let pikachu = this._http.get<PokeData>('https://pokeapi.co/api/v2/pokemon/25/');
     pikachu.subscribe(data => {
-      console.log(data);
       console.log('Got data', data.abilities)
       for (const item of data.abilities){
         console.log(item.ability.name);
+        let getInfo = this._http.get<InfoData>(item.ability.url)
+        getInfo.subscribe(data => {
+          console.log(`${data.pokemon.length} pokemon also share the ability`);
+        })
+        
       }
+     
     });
    };
 }
+
+interface InfoData {
+  pokemon: Array<number>
+}
+
 interface PokeData {
   abilities: Array<Ability>
 }
